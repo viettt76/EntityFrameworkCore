49b6d1649fb21414d4981b3b31e59af717465176
viettt76 <viettt76@gmail.com>
2024-07-23 21:01:26 +0700

    Add project files.

---
+++ HoangQuocViet/Model/FPTContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace HoangQuocViet.Model
+{
+    public class FPTContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-MFF8JAE\\SQLEXPRESS;Database=FPT;Trusted_Connection=True;");
+        }
+    }
+}
